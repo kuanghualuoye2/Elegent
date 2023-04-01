123ebadc6546dbddf5149696fbc6ab110241f4d6
liang <2278006969@qq.com>
2023-04-01 17:22:38 +0800

    添加了懒加载

--- bootstrap2/index.js
+++ bootstrap2/index.js
@@ -83,3 +83,42 @@ $(window).scroll(function () {
 $("#button").click(function(e){
     console.log(e.target);
 })
+
+$(function() {
+    // 获取所有图片元素
+    var images = $('img');
+  
+    // 遍历所有图片元素
+    images.each(function() {
+      // 添加 lazy 类
+      $(this).addClass('lazy');
+      // 设置 data-src 属性
+      $(this).attr('data-src', $(this).attr('src'));
+      // 把 src 属性设置为空
+      $(this).attr('src', '');
+    });
+  
+    // 页面滚动事件处理程序
+    function lazyload() {
+      // 获取所有带有 lazy 类的图片元素
+      var lazyloadImages = $('img.lazy');
+      if (lazyloadImages.length > 0) {
+        var scrollTop = $(window).scrollTop() + $(window).height();
+        $.each(lazyloadImages, function() {
+          var item = $(this);
+          if (item.offset().top < scrollTop) {
+            item.attr('src', item.data('src'));
+            item.removeClass('lazy');
+          }
+        });
+      }
+    }
+  
+    // 首次加载页面时加载可见的图片
+    lazyload();
+  
+    // 页面滚动事件处理程序
+    $(window).scroll(function() {
+      lazyload();
+    });
+  });
